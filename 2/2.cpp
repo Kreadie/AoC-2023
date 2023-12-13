c617ffa38907b55f49c1f595e21ee3e1c6bc1bb7
devAlexanderZ <tkid1337@gmail.com>
2023-12-14 00:33:37 +0300

    Edited

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World";
+    std::string line;
+    int sum = 0;
+    int rTotal = 12;
+    int gTotal = 13;
+    int bTotal = 14;
+    size_t counter = 0;
+    bool possible;
+
+    std::fstream File("Input data.txt");
+
+    while(std::getline(File, line))
+    {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        counter++;
+        possible = true;
+
+        for(size_t i = 7; i < line.size(); i++)
+        {
+
+        }
+    }
+
+    std::cout << sum;
+    
 }
