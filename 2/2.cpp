108df2e04d440e8cc2cce95595f6f0d59d0b2efb
devAlexanderZ <tkid1337@gmail.com>
2023-12-14 00:45:10 +0300

    Added task two, part one

--- 2/2.cpp
+++ 2/2.cpp
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <string>
 
-int main()
+
+void partOne()
 {
     std::string line;
     int sum = 0;
@@ -21,13 +22,53 @@ int main()
         int b = 0;
         counter++;
         possible = true;
+        line += ";";
 
         for(size_t i = 7; i < line.size(); i++)
         {
+            if(line[i] == ';')
+            {
+                if(r > rTotal || b > bTotal || g > gTotal)
+                {
+                    possible = false;
+                    break;
+                }
+                r = 0;
+                g = 0;
+                b = 0;
+                continue;
+            }
 
+            char firstDigit = line[i - 3];
+            char secondDigit = line[i - 2];
+            
+            if(line[i] == 'r')
+            {
+                r = std::stoi(std::string() + firstDigit + secondDigit);
+                i++;
+            }
+
+            if(line[i] == 'g')
+            {               
+                g = std::stoi(std::string() + firstDigit + secondDigit);
+                i++;
+            }
+
+            if(line[i] == 'b')
+            {
+                b = std::stoi(std::string() + firstDigit + secondDigit);
+                i++;
+            }
         }
+        if(possible)
+            sum += counter;        
     }
 
     std::cout << sum;
+}
+
+int main()
+{
+    partOne();
     
 }
